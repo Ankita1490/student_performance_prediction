a3e0d2dc61c89966b748a5f91b3cb4b681d10d1c
Ankita Pillay <ankpillay@gmail.com>
2025-01-26 12:43:43 +0530

    imported logging in exception class

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from src.logger import logging
  
  
 def error_message_details(error, error_detail:sys):
@@ -16,4 +17,5 @@ class CustomException(Exception):
         self.error_message = error_message_details(error_message, error_details )
      
     def __str__(self):
-        return self.error_message
+        return self.error_message   
+    
